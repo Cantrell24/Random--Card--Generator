3fa8933e0102a3ea9517e0e0dcac7f1098350927
Legend Andrews <139176918+Cantrell24@users.noreply.github.com>
2024-01-22 01:53:45 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,36 @@
-/* eslint-disable */
+/*eslint-disable*/
 import "bootstrap";
 import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onload = () => {
+  document.querySelector(".card").classList.add(generateRandomSuit());
+  document.querySelector(".number").innerHTML = generalRandomNumber();
+  setTimeout(() => {}, 1000);
+};
+function generalRandomNumber() {
+  let numbers = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  let randomIndex = Math.floor(Math.random() * numbers.length);
+  return numbers[randomIndex];
+}
+const generateRandomSuit = () => {
+  let suit = ["spades", "diamond", "heart", "club"];
+  let randomIndex = Math.floor(Math.random() * suit.length);
+  return suit[randomIndex];
 };
